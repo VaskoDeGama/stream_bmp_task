d1622c3c1c50626282913467f7d5d763335016bb
VaskoDeGama <vasr161095@gmail.com>
2020-10-02 00:18:48 +0300

    on pipe events and drain tests

--- tests/conver.spec.js
+++ tests/conver.spec.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises
 const path = require('path')
-const { convert, flipRow } = require('../src/core')
+const { convert, flipRow, MyWriteStream, MyReadStream } = require('../src/core')
 
 jest.setTimeout(900000)
 
@@ -102,5 +102,103 @@ describe('flip row', () => {
     const result = flipRow(testBuffer)
 
     expect(result).toStrictEqual(equalBuffer)
+  })
+})
+
+describe('STREAMS:', () => {
+  test('pipe', async () => {
+    const bigArray = Array.from({ length: 30 }, (v, k) => k + 1)
+    const readOptions = { highWaterMark: 3 }
+    const readStream = new MyReadStream(bigArray, readOptions)
+
+    const writeStream = new MyWriteStream()
+
+    readStream.pipe(writeStream)
+
+    const result = new Promise(resolve => {
+      readStream.on('close', () => {
+        const res = writeStream.getResult()
+
+        writeStream.destroy()
+        readStream.destroy()
+        resolve(res)
+      })
+    })
+
+    const arr = await result
+
+    expect(arr.length).toBe(3)
+  })
+  test('events', async () => {
+    const bigArray = Array.from({ length: 30 }, (v, k) => k + 1)
+    const readOptions = { highWaterMark: 3 }
+    const readStream = new MyReadStream(bigArray, readOptions)
+
+    const writeStream = new MyWriteStream()
+
+    readStream.on('data', (chunk) => {
+      writeStream.write(chunk)
+    })
+
+    readStream.on('end', () => {
+      writeStream.end()
+    })
+
+    const result = new Promise(resolve => {
+      readStream.on('close', () => {
+        const res = writeStream.getResult()
+
+        writeStream.destroy()
+        readStream.destroy()
+        resolve(res)
+      })
+    })
+
+    const arr = await result
+
+    expect(arr.length).toBe(3)
+  })
+  test('drain', async () => {
+    const bigArray = Array.from({ length: 30 }, (v, k) => k + 1)
+    const readOptions = { highWaterMark: 3 }
+    const writeOptions = { highWaterMark: 2 }
+    const readStream = new MyReadStream(bigArray, readOptions)
+
+    const writeStream = new MyWriteStream(writeOptions)
+
+    const writeDataOrNot = (data, cb) => {
+      if (!writeStream.write(data)) {
+        readStream.pause()
+        writeStream.once('drain', cb)
+      } else {
+        process.nextTick(cb)
+      }
+    }
+
+    const onDrain = () => {
+      readStream.resume()
+    }
+
+    readStream.on('data', (chunk) => {
+      writeDataOrNot(chunk, onDrain)
+    })
+
+    readStream.on('end', () => {
+      writeStream.end()
+    })
+
+    const result = new Promise(resolve => {
+      readStream.on('close', () => {
+        const res = writeStream.getResult()
+
+        writeStream.destroy()
+        readStream.destroy()
+        resolve(res)
+      })
+    })
+
+    const arr = await result
+
+    expect(arr.length).toBe(3)
   })
 })
